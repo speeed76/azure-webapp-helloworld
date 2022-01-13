2da862b69166a37b23b4ea31eeeff94975d24ddf
speeed76 <pawel.giers@gmail.com>
2022-01-13 22:11:47 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const sever = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end("<html><body><h1>Hello World</h1></body></html>");
+});
+
+const port = process.env.PORT || 1337;
+server.listen(port);
+console.log(`Server running at http://localhost:${port}`);
